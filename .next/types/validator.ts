c863d81daf2b859dba8e62872528475ac3bc39f4
Mohit <your-github-email@example.com>
2026-04-18 23:13:58 +0530

    feat: edit article and MCQs in drafts before publishing

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -47,6 +47,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
 }
 
 
+// Validate ../../src/app/admin/daily/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/admin/daily">> = Specific
+  const handler = {} as typeof import("../../src/app/admin/daily/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/admin/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/admin">> = Specific
